0b2a946629b877d5f7e287fee69281605f6117f9
Srijan Das <dassrijan16@gmail.com>
2024-10-24 20:47:16 +0530

    add resource routes for admin role and get routes

--- server/routes/Resource.js
+++ server/routes/Resource.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const { uploadFile, deleteResource } = require("../controllers/Resources");
-const { auth, isAdmin } = require("../middleware/auth"); 
+const { uploadFile, 
+    deleteResource, 
+    approveResource, 
+    showAllResource,
+    showResourceByCourse,
+    showApprovedResource,
+    showById,
+    showNewUploads } = require("../controllers/Resources");
+const { auth, isAdmin } = require("../middleware/auth");
 
 // POST: Upload a new resource (Only authenticated users)
 router.post("/upload", auth, uploadFile);
-router.delete('/delete/:id', auth, isAdmin, deleteResource);
+router.get("/showByCourse/:courseName", auth, showResourceByCourse);
+router.get('/showApprovedResource', auth, showApprovedResource);
+router.get('/showById/:id',auth, showById);
+
+
+//admin routes
+router.put('/approve/:id', auth, isAdmin, approveResource);
+router.delete('/delete/:id', auth, isAdmin, deleteResource); //rejectAndDelete and deleteResource both handle the same stuff , redundant?? => @Siddharth
+router.get('/all', auth, isAdmin, showAllResource);
+router.get('/showPendingResource',auth, isAdmin, showNewUploads);
 
 module.exports = router;
  
