4a21f46dfe3b42df93243c821bd7c591f5bd8da5
SiddharthJiyani <sidjiyani2003@gmail.com>
2024-11-06 12:33:55 +0530

    deploy issues solved

--- client/src/components/CalculateCgpa.jsx
+++ client/src/components/CalculateCgpa.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Input } from "@/components/ui/input";
+import { Input } from "@/components/ui/Input";
 import {
     Card,
     CardHeader,
