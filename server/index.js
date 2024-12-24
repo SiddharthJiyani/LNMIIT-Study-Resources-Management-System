524fe2262b2b70fb4a6b053080871d348090625a
SiddharthJiyani <sidjiyani2003@gmail.com>
2024-12-24 10:00:47 +0530

    update frontend url

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 // CORS configuration
 const corsOptions = {
-	origin: ["http://localhost:5173", "https://lnmiit-study-resouces-management-system.vercel.app"], // Replace with your frontend URL
+	origin: ["http://localhost:5173", "https://lnmiit-study-resouces-management-system.vercel.app","https://lnm-srms.vercel.app"], // Replace with your frontend URL
 	credentials: true,
 	optionsSuccessStatus: 200
 };
